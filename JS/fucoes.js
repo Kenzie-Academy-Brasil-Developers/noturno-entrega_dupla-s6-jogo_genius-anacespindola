ed327a428cde8946934e4b55e7ab2fcfd6ab7269
mariaritabelchior <mariaritabelchior@gmail.com>
2022-03-31 06:15:00 -0300

    adicionando alinhamento CSS e pasta com Funções

---
+++ JS/fucoes.js
@@ -0,0 +1,9 @@
+//Função que escolhe a sequencia de cores aleatóriamente
+
+//função que checa se acertou ou errou
+
+//função que exibe painel de erro se errou
+
+//função que exibe quantidade de acertos
+
+//função que exibe painel de acertos se acertou
